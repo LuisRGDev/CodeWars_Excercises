ae1b602e7f4afcd17890fb93668e4e2e9949b750
LuisRGDev <luisalbertorosalesg@gmail.com>
2025-11-18 14:34:55 -0600

    Ejercicio de Palindromo

---
+++ Is it a palindrome.js
@@ -0,0 +1,10 @@
+function isPalindrome(x) {
+  // your code here
+  let word = x.toLowerCase();
+  let wordInverse = word.split('').reverse().join('');
+  if (word === wordInverse){
+    return true;
+  }else{
+    return false;
+  }
+}
