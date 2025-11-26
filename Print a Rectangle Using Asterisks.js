acc119cc35191e7c0dd33255b2958cd5178c6841
LuisRGDev <luisalbertorosalesg@gmail.com>
2025-11-26 10:26:02 -0600

    Solucion de ejercicio imprimir un rectangulo

--- Print a Rectangle Using Asterisks.js
+++ Print a Rectangle Using Asterisks.js
@@ -1,20 +1,10 @@
 function getRectangleString(width, height) {
-  let result = "";
+  
+  const outer = "*".repeat(width) + "\r\n"
+  ,     inner = (width >= 2 ? "*" + " ".repeat(width - 2) : "") + "*\r\n";
 
-    for(let i = 0; i < width; ++i){
-        result += "* ";
-    }
-    result += "\n";
-
-    for(let j = 0; j< height; ++j){
-            result += "*" + " ".repeat(width * 2 - 3) + "*\n";
-        }
-
-    for(let i = 0; i < width; ++i){
-    result += "* ";
-    }
-
-    return result; 
+  return outer + (height < 2 ? "" : inner.repeat(height - 2) + outer);
+  
 }
 
 console.log(getRectangleString(3,3));
