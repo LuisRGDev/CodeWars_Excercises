08c4a35a850600f0d86425638d34d363acb02cff
LuisRGDev <luisalbertorosalesg@gmail.com>
2025-11-28 14:58:07 -0600

    Creacion de nueva kata Sorting the Odd way

---
+++ Sorting the Odd way!.js
@@ -0,0 +1,10 @@
+function sortItOut(array){
+//Write your own code!
+    const input = array.split(' ').map(Number);
+    const redondeados = input.map(numero => Math.floor(numero))
+    let output = "";
+
+    if(input.map % 2 !==0){
+        
+    }
+}
